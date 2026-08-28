0112a6d63e60ae76fdd97122ed841b48f921a652
jeremi.santoso@metrodata.co.id <jeremi.santoso@metrodata.co.id>
2026-08-28 07:55:17 +0000

    Update template
    Update script, config YAML

--- pipelines/dummy_sdp_pipeline/transformations/pipeline_btn_development/cfmast_pipeline.py
+++ pipelines/dummy_sdp_pipeline/transformations/pipeline_btn_development/cfmast_pipeline.py
@@ -1,8 +1,8 @@
 import dlt
 import sys
 from pyspark.sql import functions as F
-sys.path.insert(0, "/Workspace/Users/jeremi.santoso@metrodata.co.id/dbx-pipeline-integration/")
-from utils.config_load import load_table_config
+sys.path.insert(0, "/Workspace/Users/jeremi.santoso@metrodata.co.id/dbx-pipeline-integration")
+from utils.config_load import load_table_config #, get_jdbc_options (UNCOMMENT jika menggunakan source dari JDBC)
 from utils.sdp_silver_transform import transform
 
 TABLE_NAME = "customer"
@@ -10,15 +10,15 @@ config = load_table_config(TABLE_NAME)
 table_cfg = config["tables"][TABLE_NAME]
 bronze_cfg = table_cfg["bronze"]
 silver_cfg = table_cfg["silver"]
-s3_cfg = bronze_cfg["s3"]
-
+source_type = bronze_cfg["source_type"]
+source_cfg = bronze_cfg[source_type]
 
 #####################################
 ##### ---- BRONZE LAYER ----- #######
 #####################################
 
 @dlt.table(
-    name=f"bronze_s3_{TABLE_NAME}",
+    name=f"bronze_{source_type}_{TABLE_NAME}",
     cluster_by= [bronze_cfg["cluster_by_col"]] if bronze_cfg.get("cluster_by_col") else None,
     table_properties={
     "delta.dataSkippingStatsColumns": bronze_cfg.get("cluster_by_col")
@@ -26,17 +26,28 @@ s3_cfg = bronze_cfg["s3"]
 )
 
 def bronze_customer():
+    ## load data dari S3
     df_raw = (
         spark.readStream.format("cloudFiles")
-        .options(**s3_cfg["read_options"])
-        .load(s3_cfg["s3_path"])
+        .options(**source_cfg["read_options"])
+        .load(source_cfg["s3_path"])
     )
+
+    ## load data dari jdbc
+    # jdbc_cfg = source_cfg["read_options"]
+    # df_raw = (
+        # spark.read
+        # .format("jdbc")
+        # .options(**get_jdbc_options(jdbc_cfg))
+        # .load()
+    # )       
 
     return (
         df_raw
         .withColumn("ingested_timestamp", F.current_timestamp())
         .withColumn("ingested_date", F.current_date())
         .withColumn("source_file", F.col("_metadata.file_path"))
+        ## .withColumn("source_file", F.lit(jdbc_cfg["table"])) ## Pakai Source ini untuk Source dengan nama tablenya
     )
 
 #####################################
@@ -44,17 +55,17 @@ def bronze_customer():
 #####################################
 
 
-## STORE TRANSFORM TO VIEW
+## STORE TRANSFORM TO VIEW --> SEBAGAI SOURCE DATA YANG SUDAH DITRANSFORM DAN DIGUNAKAN DI FUNGSI APPLY_CHANGES
 @dlt.view(name=f"silver_{TABLE_NAME}_staging")
 ## FILTER PK tidak boleh NULL untuk kebutuhan merging di akhir.
 @dlt.expect_all_or_drop({f"valid_{pk}": f"`{pk}` IS NOT NULL" for pk in silver_cfg["primary_keys"]})
 def staged():
-    df_bronze = dlt.read_stream(f"bronze_s3_{TABLE_NAME}")
+    df_bronze = dlt.read_stream(f"bronze_{source_type}_{TABLE_NAME}")
     return transform(df_bronze, table_cfg)
 
 
 dlt.create_streaming_table(
-    name=f"silver_s3_{TABLE_NAME}",
+    name=f"silver_{source_type}_{TABLE_NAME}",
     cluster_by=[silver_cfg["partition_columns"]] if silver_cfg.get("partition_columns") else None,
     table_properties={
     "delta.dataSkippingStatsColumns": silver_cfg.get("partition_columns")
@@ -71,7 +82,7 @@ else:
     sequence_by = F.col("ingested_timestamp")
 
 dlt.apply_changes(
-    target=f"silver_s3_{TABLE_NAME}",
+    target=f"silver_{source_type}_{TABLE_NAME}",
     source=f"silver_{TABLE_NAME}_staging",
     keys=silver_cfg["primary_keys"],
     sequence_by=sequence_by,
--- utils/config_load.py
+++ utils/config_load.py
@@ -12,3 +12,30 @@ def load_config(path: str) -> dict:
 def load_table_config(table_name: str) -> dict:
     path = get_table_config_path(table_name)
     return load_config(path)
+
+def get_jdbc_options(jdbc_cfg: dict) -> dict:
+    options = {
+        "url": (
+            f"jdbc:postgresql://"
+            f"{jdbc_cfg['host']}:{jdbc_cfg['port']}/"
+            f"{jdbc_cfg['database']}?"
+            f"sslmode={jdbc_cfg['ssl_mode']}"
+        ),
+        "dbtable": jdbc_cfg["table"],
+        "driver": jdbc_cfg["driver"],
+        "user": dbutils.secrets.get(
+            scope=jdbc_cfg["secret_scope"],
+            key=jdbc_cfg["secret_key_user"]
+        ),
+        "password": dbutils.secrets.get(
+            scope=jdbc_cfg["secret_scope"],
+            key=jdbc_cfg["secret_key_password"]
+        )
+    }
+    ## Jika ada tambahan parameter untuk Partitioning
+    partition_opts = ["fetchsize", "partitionColumn", "lowerBound", "upperBound", "numPartitions"]
+    for opt in partition_opts:
+        if jdbc_cfg.get(opt) is not None:
+            options[opt] = str(jdbc_cfg[opt])
+
+    return options
